1a81fc3324a90d93488327225238f4a2aff82cf0
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2025-07-25 22:18:05 +0300

    feat: add create records table migration

---
+++ database/migrations/2025_07_25_151625_create_records_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Enums\Status;
+
+class CreateRecordsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('records', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->enum('status', [Status::ALLOWED, Status::PROHIBITED]);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('records');
+    }
+}

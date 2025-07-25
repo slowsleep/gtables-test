ddb5aa9fc133dcec8b8c0797c76099b142596d76
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2025-07-25 22:23:16 +0300

    feat: add home route with RecordController index

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RecordController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [RecordController::class, 'index'])->name('home');

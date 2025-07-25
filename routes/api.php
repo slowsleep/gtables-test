9fbb95e42348f93c6e6c7f95ef3f188dbbdff291
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2025-07-25 22:23:55 +0300

    feat: add API routes for generating and deleting all records

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/generate', [App\Http\Controllers\Api\RecordController::class, 'generate']);
+Route::delete('/destroy-all', [App\Http\Controllers\Api\RecordController::class, 'destroyAll']);

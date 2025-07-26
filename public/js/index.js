4ec3974c80127364e4eebb439bf47cae4d7d5ce3
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2025-07-26 16:36:20 +0300

    feat: implement UI for Record model CRUD operations

---
+++ public/js/index.js
@@ -0,0 +1,207 @@
+// cоздание новой записи
+const createRecordForm = document.getElementById('create-record-form');
+createRecordForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    try {
+        const response = await fetch('/api/record', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                title: createRecordForm.title.value,
+                status: createRecordForm.status.value
+            })
+        })
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+        const json = await response.json();
+        const record = json.record;
+        const table = document.querySelector('tbody');
+        const row = document.createElement('tr');
+        row.classList.add('odd:bg-neutral-200', 'dark:odd:bg-neutral-600');
+        row.innerHTML = `
+            <td>${record.id}</td>
+            <td>${record.title}</td>
+            <td>${record.status}</td>
+            <td><button
+            class="edit-record bg-blue-400 hover:bg-blue-800 text-white font-bold py-1 px-2 m-1 rounded hover:cursor-pointer"
+            data-record-id="${record.id}">редактировать</button></td>
+            <td><button
+            class="delete-record bg-red-400 hover:bg-red-800 text-white font-bold py-1 px-2 m-1 rounded hover:cursor-pointer"
+            data-record-id="${record.id}">удалить</button></td>
+        `;
+        table.appendChild(row);
+    } catch (error) {
+        console.error(error.message);
+    }
+
+})
+
+// генерирование 1000 записей
+const generateButton = document.getElementById('generate-records');
+generateButton.addEventListener('click', async () => {
+    await getData();
+})
+
+async function getData() {
+    try {
+        const response = await fetch('/api/records/generate', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                count: 1000
+            })
+        });
+        if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+        }
+
+        const json = await response.json();
+        const records = json.records;
+        const table = document.querySelector('tbody');
+
+        records.forEach(record => {
+            const row = document.createElement('tr');
+            row.classList.add('odd:bg-neutral-200', 'dark:odd:bg-neutral-600');
+            row.innerHTML = `
+                <td>${record.id}</td>
+                <td>${record.title}</td>
+                <td>${record.status}</td>
+                <td><button
+                class="edit-record bg-blue-400 hover:bg-blue-800 text-white font-bold py-1 px-2 m-1 rounded hover:cursor-pointer"
+                data-record-id="${record.id}">редактировать</button></td>
+                <td><button
+                class="delete-record bg-red-400 hover:bg-red-800 text-white font-bold py-1 px-2 m-1 rounded hover:cursor-pointer"
+                data-record-id="${record.id}">удалить</button></td>
+            `;
+            table.appendChild(row);
+        });
+
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+
+
+// удаление всех записей
+const deleteButton = document.getElementById('delete-records');
+deleteButton.addEventListener('click', async () => {
+    await deleteData();
+})
+
+async function deleteData() {
+    try {
+        const response = await fetch('/api/records/destroy-all', {
+            method: 'DELETE'
+        });
+        if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+        }
+        const table = document.querySelector('tbody');
+        table.innerHTML = '';
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+
+// открытие модального окна с данными записи для редактирования записи
+async function openEditModal(recordId) {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+    let editForm = modal.querySelector("form[name='edit-form']");
+    let textId = editForm.querySelector("#modal-record-id");
+    let inputTitle = editForm.querySelector("#modal-record-title");
+    let inputStatus = editForm.querySelector("#modal-record-status");
+
+    const response = await fetch('/api/record/' + recordId);
+
+    if (response.ok) {
+        let data = await response.json();
+        let record = data.record;
+        textId.innerHTML = record.id.toString();
+        inputTitle.value = record.title;
+        inputStatus.value = record.status.toLowerCase();
+    } else {
+        alert(response.statusText);
+    }
+
+    let saveButton = editForm.querySelector("button[name='save']");
+    let cancelButton = editForm.querySelector("button[name='cancel']");
+
+    // убираем старые обработчики (если повторно откроем модалку)
+    const saveClone = saveButton.cloneNode(true);
+    saveButton.parentNode.replaceChild(saveClone, saveButton);
+
+    const cancelClone = cancelButton.cloneNode(true);
+    cancelButton.parentNode.replaceChild(cancelClone, cancelButton);
+
+    cancelClone.addEventListener('click', () => {
+        modal.classList.add('hidden');
+        overlay.classList.add('hidden');
+    });
+
+    saveClone.addEventListener('click', async (event) => {
+        event.preventDefault();
+
+        const response = await fetch('/api/record/' + recordId, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                title: inputTitle.value,
+                status: inputStatus.value
+            })
+        });
+
+        if (response.ok) {
+            modal.classList.add('hidden');
+            overlay.classList.add('hidden');
+            location.reload();
+        } else {
+            alert(response.statusText);
+        }
+    });
+}
+
+// обработка кликов по таблице
+// для обработки кнопок редактирования и удаления
+const tableBody = document.querySelector('tbody');
+
+tableBody.addEventListener('click', async (event) => {
+    const target = event.target;
+
+    // редактирование записи
+    if (target.classList.contains('edit-record')) {
+        const recordId = target.getAttribute('data-record-id');
+        // показ модалки + загрузка данных
+        await openEditModal(recordId);
+    }
+
+    // удаление записи
+    if (target.classList.contains('delete-record')) {
+        const recordId = target.getAttribute('data-record-id');
+        const isDelete = confirm("Вы уверены, что хотите удалить запись?");
+        if (isDelete) {
+            await deleteRecord(recordId);
+        }
+    }
+});
+
+async function deleteRecord(recordId) {
+    try {
+        const response = await fetch('/api/record/' + recordId, {
+            method: 'DELETE'
+        });
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+        location.reload();
+    } catch (error) {
+        console.error(error.message);
+    }
+}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,6 +22,21 @@
         <div class="flex items-top justify-center min-h-screen bg-neutral-100 dark:bg-neutral-900 py-4 sm:pt-0">
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8 py-4 flex flex-col gap-2">
                 <h1 class="text-3xl text-neutral-900 dark:text-neutral-100">синхронизация с google tables</h1>
+                <form class="flex flex-col gap-2 bg-neutral-700 dark:bg-neutral-800 p-4 text-neutral-100" id="create-record-form">
+                    <h2>Создать запись</h2>
+                    <div class="flex flex-row broder-white-1">
+                        <label class="w-1/4" for="title">title</label>
+                        <input class="border border-gray-500 w-full" type="text" name="title" id="title">
+                    </div>
+                    <div class="flex flex-row broder-white-1">
+                        <label class="w-1/4" for="status">status</label>
+                        <select class="border border-gray-500 w-full" name="status" id="status">
+                            <option value="allowed">allowed</option>
+                            <option value="prohibited">prohibited</option>
+                        </select>
+                    </div>
+                    <input type="submit" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded hover:cursor-pointer" value="Создать" />
+                </form>
                 <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded hover:cursor-pointer" id="generate-records">Сгенерировать 1000 записей</button>
                 <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded hover:cursor-pointer" id="delete-records">Удалить все записи</button>
                 <table class="table-auto w-full bg-neutral-100 dark:bg-neutral-700">
@@ -34,77 +49,44 @@
                     </thead>
                     <tbody>
                         @foreach ($records as $record)
-                            <tr class="odd:bg-neutral-200 dark:odd:bg-neutral-600">
-                                <td>{{ $record->id }}</td>
+                            <tr class="odd:bg-neutral-200 dark:odd:bg-neutral-600" data-key="{{ $record->id }}">
+                                <td data-record-id="{{$record->id}}">{{ $record->id }}</td>
                                 <td>{{ $record->title }}</td>
                                 <td>{{ $record->status }}</td>
+                                <td><button
+                                    class="edit-record bg-blue-400 hover:bg-blue-800 text-white font-bold py-1 px-2 m-1 rounded hover:cursor-pointer"
+                                    data-record-id="{{$record->id}}">редактировать</button></td>
+                                <td><button
+                                    class="delete-record bg-red-400 hover:bg-red-800 text-white font-bold py-1 px-2 m-1 rounded hover:cursor-pointer"
+                                    data-record-id="{{$record->id}}">удалить</button></td>
                             </tr>
                         @endforeach
                     </tbody>
                 </table>
             </div>
         </div>
+        <div id="modal" class="fixed left-[50%] top-[50%] transform -translate-x-1/2 -translate-y-1/2 z-5 bg-neutral-100 dark:bg-neutral-900 p-3 text-neutral-900 dark:text-neutral-100 border-1 border-blue-500 hidden">
+            <form name="edit-form" class="flex flex-col">
+                <h1>Редактирование записи</h1>
+                <p>id: <span id="modal-record-id"></span></p>
+                <div class="flex flex-row justify-between">
+                    <p>title: </p>
+                    <input class="border border-gray-500" type="text" placeholder="title" name="title" id="modal-record-title" />
+                </div>
+                <div class="flex flex-row gap-2">
+                    <p>status: </p>
+                    <select class="border border-gray-500" name="status" id="modal-record-status">
+                        <option value="allowed">allowed</option>
+                        <option value="prohibited">prohibited</option>
+                    </select>
+                </div>
+                <div class="flex flex-row justify-between">
+                    <button type="submit" name="save" class="bg-green-500 hover:bg-green-700 text-white font-bold p-1 rounded hover:cursor-pointer">сохранить</button>
+                    <button type="button" name="cancel" class="bg-gray-500 hover:bg-gray-700 text-white font-bold p-1 rounded hover:cursor-pointer">отмена</button>
+                </div>
+            </form>
+        </div>
+        <div class="fixed top-0 left-0 w-full h-full bg-black opacity-50 z-4 hidden" id="overlay"></div>
     </body>
-    <script>
-        const generateButton = document.getElementById('generate-records');
-        generateButton.addEventListener('click', async () => {
-            await getData();
-        })
-
-        async function getData() {
-            try {
-                const response = await fetch('/api/generate', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({
-                        count: 1000
-                    })
-                });
-                if (!response.ok) {
-                throw new Error(`Response status: ${response.status}`);
-                }
-
-                const json = await response.json();
-                const records = json.records;
-                const table = document.querySelector('tbody');
-
-                records.forEach(record => {
-                    const row = document.createElement('tr');
-                    row.classList.add('odd:bg-neutral-200', 'dark:odd:bg-neutral-600');
-                    row.innerHTML = `
-                        <td>${record.id}</td>
-                        <td>${record.title}</td>
-                        <td>${record.status}</td>
-                    `;
-                    table.appendChild(row);
-                });
-
-            } catch (error) {
-                console.error(error.message);
-            }
-        }
-
-        const deleteButton = document.getElementById('delete-records');
-        deleteButton.addEventListener('click', async () => {
-            await deleteData();
-        })
-
-        async function deleteData() {
-            try {
-                const response = await fetch('/api/destroy-all', {
-                    method: 'DELETE'
-                });
-                if (!response.ok) {
-                throw new Error(`Response status: ${response.status}`);
-                }
-                const table = document.querySelector('tbody');
-                table.innerHTML = '';
-            } catch (error) {
-                console.error(error.message);
-            }
-        }
-
-    </script>
+    <script src="{{ asset('js/index.js') }}"></script>
 </html>

d6c7e35cb5518df74dfcb409dec82111f7d52323
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2025-07-30 23:45:04 +0300

    feat: add google credentials config to services.php

--- config/services.php
+++ config/services.php
@@ -30,4 +30,8 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'google' => [
+        'credentials_path' => storage_path('app/google/credentials.json'),
+    ],
+
 ];

cff9f5ffa33e3378e6c8cbe46d6c6bfe72e24339
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2025-07-31 19:45:35 +0300

    feat: add allowed scope to Record model

--- app/Models/Record.php
+++ app/Models/Record.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Enums\Status;
 
 class Record extends Model
 {
@@ -14,4 +15,8 @@ class Record extends Model
         'status',
     ];
 
+    public function scopeAllowed()
+    {
+        return $this->where('status', Status::ALLOWED);
+    }
 }

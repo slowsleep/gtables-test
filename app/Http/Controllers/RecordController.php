2ef640b6d60bb1161d8508cb9729fc5ce8c02a04
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2025-07-31 20:00:16 +0300

    feat: pass Google Spreadsheet ID or false to welcome view

--- app/Http/Controllers/RecordController.php
+++ app/Http/Controllers/RecordController.php
@@ -4,12 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Record;
+use App\Models\GoogleSheetSettings;
 
 class RecordController extends Controller
 {
     public function index()
     {
         $records = Record::all();
-        return view('welcome', ['records' => $records]);
+        $googleSheetIdIsSet = GoogleSheetSettings::first();
+        $googleSheetIdIsSet = $googleSheetIdIsSet ? $googleSheetIdIsSet->spreadsheet_id : false;
+        
+        return view('welcome', ['records' => $records, 'googleSheetIdIsSet' => $googleSheetIdIsSet]);
     }
 }
